90fecb9427b8d65fa17446de6ede3466b3e4bdfa
WebertSz <trebew2012@gmail.com>
2023-11-02 14:47:07 -0300

    atualização aula função

---
+++ Aulas/aulaFunction.js
@@ -0,0 +1,11 @@
+torrar();
+
+function torrar(){
+    console.log("Torrando pão...")
+    injetarPao();
+}
+
+function injetarPao(){
+    console.log("Preparando para injetar o pão...")
+    console.log("Finalizado!")
+}
