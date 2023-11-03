629e387958da049bf9f9b8f6429caca19f1605ff
WebertSz <trebew2012@gmail.com>
2023-11-03 17:33:17 -0300

    atualização aula função

--- Aulas/aulaFunction.js
+++ Aulas/aulaFunction.js
@@ -1,11 +1,29 @@
-torrar();
+let userName = getFirstName("Pedro Silva")
 
-function torrar(){
-    console.log("Torrando pão...")
-    injetarPao();
+createStringConnection("db_products", userName, "12345")
+torrar("pão de forma");
+let result = soma(5, 5)
+console.log(`O valor total é ${result}`)
+
+function torrar(pão){
+    console.log(`Torrando ${pão}`)
+    injetarPao(pão);
 }
 
-function injetarPao(){
-    console.log("Preparando para injetar o pão...")
+function injetarPao(pão){
+    console.log(`Preparando para injetar o ${pão}`)
     console.log("Finalizado!")
+}
+
+function soma(a, b){
+    return a + b
+}
+
+function createStringConnection(databaseName, user, pass){
+    console.log(`connect:DBCONNECT;user=${user};pass=${pass};initial_database=${databaseName}`)
+}
+
+function getFirstName(name){
+    let firstName = name.split(" ")[0]
+    return firstName
 }
