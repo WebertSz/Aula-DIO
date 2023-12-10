806ad1048c916a67f8c94029ee913c52e0749356
WebertSz <trebew2012@gmail.com>
2023-12-10 16:04:55 -0300

    atualização da shadow dom, criação de componentes e estilização

--- News/assets/js/cardNews.js
+++ News/assets/js/cardNews.js
@@ -3,18 +3,89 @@ class CardNews extends HTMLElement {
         super();
 
         const shadow = this.attachShadow({mode: "open"});
-        
-        const componentRoot = document.createElement("h1");
-        componentRoot.textContent = this.getAttribute("noticia");
+        shadow.appendChild(this.build());
+        shadow.appendChild(this.style());
+    }
 
+    build () {
+        const componentRoot = document.createElement("div");
+        componentRoot.setAttribute("class", "card");        
+
+        const cardLeft = document.createElement("div");
+        cardLeft.setAttribute("class", "card-left");
+        
+        const autor = document.createElement("span");
+        autor.textContent = "By " + (this.getAttribute("autor") || "Anonymous");
+        
+        const linkTitle = document.createElement("a");
+        linkTitle.textContent = this.getAttribute("title");
+        linkTitle.href = this.getAttribute("link-url");
+
+        const newsContent = document.createElement("p");
+        newsContent.textContent = this.getAttribute("content");
+
+        cardLeft.appendChild(autor);
+        cardLeft.appendChild(linkTitle);
+        cardLeft.appendChild(newsContent);
+
+        const cardRight = document.createElement("div");
+        cardRight.setAttribute("class", "card-right");        
+
+        const newsImage = document.createElement("img");
+        newsImage.src = "./assets/img/bitcoin.jpeg";
+        newsImage.alt = "imagem da criptomoeda bitcoin";
+        cardRight.appendChild(newsImage);
+
+        componentRoot.appendChild(cardLeft);
+        componentRoot.appendChild(cardRight);        
+
+        return componentRoot;
+    }
+
+    style () {
+        
         const style = document.createElement("style");
-        style.textContent = `
-            h1 {
-                color: red;
-            }
-        `
-        shadow.appendChild(componentRoot);
-        shadow.appendChild(style);
+        style.textContent=`
+        .card {
+            width: 720px;
+            display: flex;
+            flex-direction: row;
+            justify-content: space-between;
+            box-shadow: 10px 10px 10px 3px rgba(0,0,0,0.75);
+            -webkit-box-shadow: 10px 10px 10px 3px rgba(0,0,0,0.75);
+            -moz-box-shadow: 10px 10px 10px 3px rgba(0,0,0,0.75);
+        }
+        
+        .card-left {
+            display: flex;
+            flex-direction: column;
+            justify-content: center;
+            padding-left: 10px;
+        }
+        
+        .card-left > span {
+            font-weight: 400;
+        }
+        
+        .card-left > a {
+            margin-top: 15px;
+            font-size: 25px;
+            color: black;
+            text-decoration: none;
+            cursor: pointer;
+            font-weight: bold;
+        }
+        
+        .card-left > p {
+            color: gray;
+        }
+        
+        .card-right > img {
+            width: 99%;
+        }
+        `;
+
+        return style;
     }
 }
 
